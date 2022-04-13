1044f96a7de23a524957661878d18dad3cfba0dd
Bulat <bulat1987@gmail.com>
2022-04-13 22:09:11 +0500

    add pow()

--- java/main/TestClass.java
+++ java/main/TestClass.java
@@ -4,5 +4,10 @@ public class TestClass {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+        System.out.println(pow());
+    }
+
+    public static int pow(){
+        return 10;
     }
 }
