55c60532eb1402629cc276d1e84a105689e968ed
Bulat <bulat1987@gmail.com>
2022-04-13 22:17:02 +0500

    implement min functions

--- java/main/TestClass.java
+++ java/main/TestClass.java
@@ -5,9 +5,15 @@ public class TestClass {
 
         System.out.println("Hello world");
         System.out.println(pow());
+        System.out.println(min(5, 8));
+        System.out.println(min(6, 3));
     }
 
     public static int pow() {
         return 10;
+    }
+
+    public static int min(int a, int b) {
+        return Math.min(a, b);
     }
 }
